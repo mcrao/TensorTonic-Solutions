7a21f18bdd15e7c4e17fe2577b32a8980b75e7ed
Manoj Chandrashekar <manoj.7892194285@gmail.com>
2026-06-28 01:18:49 -0500

    Solve relu-activation

---
+++ relu-activation/relu-activation.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def relu(x):
+    """
+    Implement ReLU activation function.
+    """
+    # Write code here
+    x = np.array(x)
+
+    return np.where(
+        x > 0,
+        x,
+        0
+    )
