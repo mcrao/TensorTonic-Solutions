e27efade93c08d244f845609704c2457eda30a39
Manoj Chandrashekar <manoj.7892194285@gmail.com>
2026-08-01 02:48:33 -0500

    Solve sigmoid-numpy

--- sigmoid-numpy/sigmoid-numpy.py
+++ sigmoid-numpy/sigmoid-numpy.py
@@ -6,9 +6,8 @@ def sigmoid(x):
     """
     # Write code here
     x = np.array(x)
-
     return np.where(
         x >= 0,
         1/(1 + np.exp(-x)),
-        np.exp(x) / (1 + np.exp(x))
+        np.exp(x)/(1 + np.exp(x))
     )
